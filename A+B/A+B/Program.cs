c0eea4d68087064678c22cf0f17bc7ca189942ce
GlushenkoBorys <borisss@list.ru>
2019-11-11 15:43:31 +0200

    Started working after a break

--- A+B/A+B/Program.cs
+++ A+B/A+B/Program.cs
@@ -18,17 +18,31 @@ namespace A_B
                 using (StreamReader sr = new StreamReader(path))
                 {
                     Console.WriteLine(sr.ReadToEnd());
-                    int [] my = new int [5];
-                    int len = 5;
-                    for (int i = 0; i < len; i++)
-                    {
-                        my[i] = sr.Peek();
-                        Console.WriteLine(my[i]);
-                    }
-                    
-                    Console.WriteLine(my);
+                    //int [] my = new int [5];
+                    //int len = 5;
+                    //for (int i = 0; i < len; i++)
+                    //{
+                    //    my[i] = sr.Peek();
+                    //    Console.WriteLine(my[i]);
+                    //}
 
-                  
+                    //Console.WriteLine(my);
+                    //bool n;
+                    //string b;
+                    //n = sr.ReadLine() is string;
+                    //b = sr.ReadToEnd();
+                    //Console.WriteLine(n);
+                    //Console.WriteLine(b);
+                    string my = sr.ReadToEnd();
+
+                    string text = sr.ReadLine();
+                    string[] words = my.Split(new char[] { ' ' });
+
+                    foreach (string s in words)
+                    {
+                        Console.WriteLine(s);
+                    }
+
                 }
             }
             catch (Exception e)
